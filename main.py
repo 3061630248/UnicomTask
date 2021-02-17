6b588d3d54c0dcbfab321f931d5b7ad9cc1fa9d3
3061630248 <79186548+3061630248@users.noreply.github.com>
2021-02-17 14:07:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -237,10 +237,10 @@ def pointsLottery_task():
         num = 0
         #如果用户未设置此值，将不会自动抽奖
         if os.environ.get('LOTTERY_NUM') != None:
-            num = os.environ.get('LOTTERY_NUM')
+            num = int(os.environ.get('LOTTERY_NUM'))
         else:
             num = 0
-        for i in range(int(num)):
+        for i in range(num):
             #用积分兑换抽奖机会
             client.get('https://m.client.10010.com/dailylottery/static/integral/duihuan?goldnumber=10&banrate=30&usernumberofjsp=' + numjsp)
             #进行抽奖
